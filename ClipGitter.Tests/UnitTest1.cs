c20870f5e3402166a4212eb519b2866ea3fb47ab
Gero Hillebrandt <ghillebrandt@gmail.com>
2024-11-01 19:09:11 +0100

    test: init test framework

---
+++ ClipGitter.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using ClipGitter;
+
+namespace ClipGitter.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestLoggerFactory_CreatesLoggerWithCustomFormatter()
+        {
+        }
+    }
+}
